3203b889c6ad6b1078f859fe1cce38b001b163df
ragmer09toske <retsepile.raymondshao@gmail.com>
2024-02-15 12:50:38 -0700

    first commit

--- components/Network.tsx
+++ components/Network.tsx
@@ -4,12 +4,14 @@ export const Network = () => {
   return (
     <div className='flex w-full gap-3 justify-center items-center h-full fixed bottom-7'>
         <div style={{
-                    backdropFilter: "blur(5px)",
-                    background: "rgba(255, 255, 255, 0.064)",
-                    borderRadius: "30px 30px 30px 30px",
-                    width: "60%",
-                    height: "80%"
-                }}>
+            backdropFilter: "blur(5px)",
+            background: "rgba(255, 255, 255, 0.064)",
+            borderWidth: "1px",
+            borderColor: "rgba(255, 255, 255, 0.164)",
+            borderRadius: "25px 25px 25px 25px",
+            width: "60%",
+            height: "80%"
+        }}>
         </div>
     </div>
   )
