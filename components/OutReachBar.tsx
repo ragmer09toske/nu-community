d49588bdf0077bdfa6d3e7c502a79d861dca103a
ragmer09toske <retsepile.raymondshao@gmail.com>
2024-02-15 08:47:34 -0700

    first commit

--- components/OutReachBar.tsx
+++ components/OutReachBar.tsx
@@ -1,14 +1,31 @@
 import React from 'react'
+import { ToggleGroup, ToggleGroupItem } from './ui/toggle-group'
+import { Inbox, MessageCircle, Send } from 'lucide-react'
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from './ui/tooltip'
 
 export const OutReachBar = () => {
   return (
-    <div className='flex w-full gap-3 justify-center fixed bottom-0'>
-        <div className='p-5'style={{
+    <div className='flex w-full gap-3 justify-center fixed bottom-7' >
+        <div className='p-1'style={{
             backdropFilter: "blur(5px)",
             background: "rgba(255, 255, 255, 0.064)",
-            borderRadius: "5px 5px 30px 30px",
+            borderRadius: "30px",
+            width: "200px"
         }}>
-            hello world
+           <ToggleGroup type="single">
+                <ToggleGroupItem value="a">
+                <TooltipProvider>
+                    <Tooltip>
+                        <TooltipTrigger><MessageCircle /></TooltipTrigger>
+                        <TooltipContent>
+                            <p>send us a message</p>
+                        </TooltipContent>
+                    </Tooltip>
+                </TooltipProvider>
+                </ToggleGroupItem>
+                <ToggleGroupItem value="b"><Send /></ToggleGroupItem>
+                <ToggleGroupItem value="c"><Inbox /></ToggleGroupItem>
+           </ToggleGroup>
         </div>
     </div>
   )
