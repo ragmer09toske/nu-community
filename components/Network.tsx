7f9f5253fd4f1fea42d99a85ca3e686c5e0be392
ragmer09toske <retsepile.raymondshao@gmail.com>
2024-02-16 00:43:58 -0700

    first commit

--- components/Network.tsx
+++ components/Network.tsx
@@ -8,17 +8,20 @@ import { Input } from './ui/input'
 
 export const Network = () => {
   return (
-    <div className='flex w-full gap-3 justify-center items-center h-full fixed bottom-7'>
+    <div className='flex w-full gap-3 justify-center top-5 items-center h-full fixed bottom-7'>
         <Card style={{
             backdropFilter: "blur(5px)",
+            background: "#09090bd9",
             borderWidth: "1px",
             width: "60%",
-            height: "80%"
+            height: "85%"
         }}
           className="flex justify-center "
         >
           <div className='w-1/2 h-full p-2 ' style={{
             backdropFilter: "blur(5px)",
+            background: "#09090bd9",
+
             borderWidth: "1px",
             borderLeftColor: "rgba(255, 255, 255, 0.164)",
             borderRightColor: "rgba(255, 255, 255, 0.164)",
@@ -26,14 +29,14 @@ export const Network = () => {
             borderBottomWidth: "0px",
             width: "60%",
           }}>
-              <Command className="rounded-lg border shadow-md p-2" style={{borderColor: "none", borderWidth: "0px"}}>
+              <Command className="rounded-lg border shadow-md p-2" style={{borderColor: "none", background: "#09090bd9", borderWidth: "0px"}}>
                 <div className='p-4
                  '>
                   <Input placeholder="Type a command or search..." />
                 </div>
                 <ScrollArea style={{borderWidth: "0px"}} className="h-[100%] flex flex-col gap-5 w-full rounded-md border p-4">
                   <div className='flex flex-col gap-5'>
-                  <Card className="w-[100%]">
+                  <Card className="w-[100%]" style={{ background: "#09090bd9"}}>
                     <CardHeader>
                       <CardTitle>Create project</CardTitle>
                       <CardDescription>Deploy your new project in one-click.</CardDescription>
