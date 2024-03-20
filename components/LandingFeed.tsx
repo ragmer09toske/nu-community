acee0028086c32783b37edb009bb6a5de0ee314c
kali <kali@kali.kali>
2024-03-21 00:26:13 +0200

    one

--- components/LandingFeed.tsx
+++ components/LandingFeed.tsx
@@ -16,104 +16,107 @@ export const LandingFeed = () => {
         borderTopWidth: "0px",
         borderBottomWidth: "0px", 
     }}>       
+      <Command className="rounded-lg border shadow-md p-2" style={{ background: "rgba(255, 255, 255, 0)", borderWidth: "0px"}}>
+        <h4 className="scroll-m-20 p-2 pl-5 text-xl font-extrabold tracking-tight lg:text-xl">
+            {/* Community Of Creatives */}
+        </h4>
+        {!isDesktop && <div className="p-5 mt-40 lg:mt-0">
+          <Image
+              src="/one.png"
+              alt="Nucleus Logo"
+              className="relative lg:dark:drop-shadow-[0_0_0.3rem_#ffffff70]"
+              width={120}
+              height={24}
+              priority
+              // hello world
+          />
+        </div>}
+        
+        <ScrollArea style={{borderWidth: "0px"}} className="h-[100%] flex flex-col gap-5 w-full rounded-md border p-4">
+          <div className='flex flex-col  gap-5'>
+          <Link href="/codinginitiative">
+          <Card className="relative w-[100%]">
+            <CardHeader>
+                <CardTitle>Welcome to our Codiac Initiative</CardTitle>
+                <CardDescription>
+                  We believe that everyone should have the opportunity to learn how to code and to create innovative solutions.
+                  <br/><br/>
+                  Get the opportunity to have your portfolio online or your business online
+                </CardDescription>
+            </CardHeader>
+            <Image
+                src="/nerdish.jpg"
+                alt="Nucleus Logo"
+                style={{
+                  borderRadius:"0 0 8px 8px"
+                }}
+                width={855}
+                height={24}
+                priority
+            />
+            <div className='absolute bottom-14 lg:bottom-44 lg:left-10 left-5'>
+              <ShimmerButton />  
+            </div>
+          </Card>
+          </Link>
 
-          <Command className="rounded-lg border shadow-md p-2" style={{ background: "rgba(255, 255, 255, 0)", borderWidth: "0px"}}>
-            <h4 className="scroll-m-20 p-2 pl-5 text-xl font-extrabold tracking-tight lg:text-xl">
-                {/* Community Of Creatives */}
-            </h4>
-            {!isDesktop && <div className="p-5 mt-40 lg:mt-0">
+          <Card className="w-[100%] p-0.5 pl-3 ">
+            <div>
+                <CardHeader>
+                    <CardTitle>We Embrace the Art of Deverse Engineering</CardTitle>
+                    <CardDescription>We unlocked the secrets of seamless transitions from Mobile to Desktop development.</CardDescription>
+                </CardHeader>
+                <div className="w-[100%] p-0.5 pl-3 flex justify-center">
+                <Image
+                    src="/mainIcons.svg"
+                    alt="Nucleus Logo"
+                    className="relative lg:dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+                    width={520}
+                    height={24}
+                    priority
+                />
+                </div>
+            </div>
+          </Card>
+
+          <Card className="w-[100%]">
+            <CardHeader>
+                <CardTitle>Elevate Your Projects with our Innovative Designing Team</CardTitle>
+                <CardDescription>Experience the fusion of creativity and technical expertise.</CardDescription>
+            </CardHeader>
               <Image
-                  src="/one.png"
-                  alt="Nucleus Logo"
-                  className="relative lg:dark:drop-shadow-[0_0_0.3rem_#ffffff70]"
-                  width={120}
-                  height={24}
-                  priority
-                  // hello world
-              />
-            </div>}
-            
-            <ScrollArea style={{borderWidth: "0px"}} className="h-[100%] flex flex-col gap-5 w-full rounded-md border p-4">
-              <div className='flex flex-col  gap-5'>
-              <Link href="/codinginitiative">
-              <Card className="relative w-[100%]">
-                <CardHeader>
-                    <CardTitle>Welcome to our Coding Initiative</CardTitle>
-                    <CardDescription>We believe that everyone should have the opportunity to learn how to code and to create innovative solutions. That&apos;s why we&apos;re thrilled to introduce our Coding Initiative, an empowering program designed to help individuals and startups on digital transformation through coding.</CardDescription>
-                </CardHeader>
-                <Image
-                    src="/nerdish.jpg"
-                    alt="Nucleus Logo"
-                    style={{
-                      borderRadius:"0 0 8px 8px"
-                    }}
-                    width={855}
-                    height={24}
-                    priority
-                />
-                <div className='absolute bottom-14 lg:bottom-44 lg:left-10 left-5'>
-                  <ShimmerButton />  
-                </div>
-              </Card>
-              </Link>
+                src="/loading-overview.avif"
+                // className="relative "
+                alt="Nucleus Logo"
+                width={855}
+                style={{
+                  borderRadius:"0 0 8px 8px"
+                }}
+                height={24}
+                priority
+            />
 
-              <Card className="w-[100%] p-0.5 pl-3 ">
-                <div>
-                    <CardHeader>
-                        <CardTitle>We Embrace the Art of Deverse Engineering</CardTitle>
-                        <CardDescription>We unlocked the secrets of seamless transitions from Mobile to Desktop development.</CardDescription>
-                    </CardHeader>
-                    <div className="w-[100%] p-0.5 pl-3 flex justify-center">
-                    <Image
-                        src="/mainIcons.svg"
-                        alt="Nucleus Logo"
-                        className="relative lg:dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-                        width={520}
-                        height={24}
-                        priority
-                    />
-                    </div>
-                </div>
-              </Card>
+          </Card>
 
-              <Card className="w-[100%]">
-                <CardHeader>
-                    <CardTitle>Elevate Your Projects with our Innovative Designing Team</CardTitle>
-                    <CardDescription>Experience the fusion of creativity and technical expertise.</CardDescription>
-                </CardHeader>
-                  <Image
-                    src="/loading-overview.avif"
-                    // className="relative "
-                    alt="Nucleus Logo"
-                    width={855}
-                    style={{
-                      borderRadius:"0 0 8px 8px"
-                    }}
-                    height={24}
-                    priority
-                />
-
-              </Card>
-
-              <Card className="w-[100%]">
-                <CardHeader>
-                    <CardTitle>Elevate Your Brand with Exquisite Typography</CardTitle>
-                    <CardDescription>Take pleasure in the intricate craftsmanship of each character, enhancing your brand&apos;s story.</CardDescription>
-                </CardHeader>
-                <Image
-                    src="/fill-container.avif"
-                    alt="Nucleus Logo"
-                    style={{
-                      borderRadius:"0 0 8px 8px"
-                    }}
-                    width={855}
-                    height={24}
-                    priority
-                />
-              </Card>
-              </div>
-            </ScrollArea>
-          </Command>
-      </div>
+          <Card className="w-[100%]">
+            <CardHeader>
+                <CardTitle>Elevate Your Brand with Exquisite Typography</CardTitle>
+                <CardDescription>Take pleasure in the intricate craftsmanship of each character, enhancing your brand&apos;s story.</CardDescription>
+            </CardHeader>
+            <Image
+                src="/fill-container.avif"
+                alt="Nucleus Logo"
+                style={{
+                  borderRadius:"0 0 8px 8px"
+                }}
+                width={855}
+                height={24}
+                priority
+            />
+          </Card>
+          </div>
+        </ScrollArea>
+      </Command>
+  </div>
   )
 }
--- components/RegisterCodingInitiative.tsx
+++ components/RegisterCodingInitiative.tsx
@@ -1,20 +1,61 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { cn } from "../app/utils/cn";
 import {
   IconBrandGithub,
   IconBrandGoogle,
   IconBrandOnlyfans,
 } from "@tabler/icons-react";
+
 import { Label } from "@radix-ui/react-label";
 import { Input } from "./ui/input";
 import { Textarea } from "./ui/textarea";
+import axios from "axios";
+import { useToast } from "./ui/use-toast"
+import { Loader } from "lucide-react";
 
 export function RegisterCodingInitiative() {
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    console.log("Form submitted");
-  };
+  const [firstname, setFirstName] = useState<string>("")
+  const [lastname, setLastname] = useState<string>("")
+  const [number,setNumber] = useState<number>()
+  const [email,setEmail] = useState<string>("")
+  const [reason, setReason] = useState<string>("")
+  const [loading, setLoading] = React.useState<boolean>(false)
+  const { toast } = useToast()
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      try {
+        setLoading(true)
+        const response = await axios.post(
+          'http://localhost:8000/codiac/register',
+          {
+            firstname: firstname,
+            lastname: lastname,
+            number: number,
+            email: email,
+            reason: reason,
+          }
+        );
+
+        toast({
+          title: "Registration succeeded",
+          description: "You will get an email for your RSV",
+        })
+
+        console.log(response.data)
+  
+        setLoading(false)
+      } catch (error) {
+        console.log('Error:', error);
+        toast({
+          title: "Registration failed",
+          description: "try to register again",
+        })
+        setLoading(false)
+      }
+    };
+
   return (
     <div className="max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-white dark:bg-black">
       <h2 className="font-bold text-xl text-neutral-800 dark:text-neutral-200">
@@ -28,20 +69,25 @@ export function RegisterCodingInitiative() {
         <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
           <LabelInputContainer>
             <Label htmlFor="firstname">First name</Label>
-            <Input id="firstname" placeholder="Khotso" type="text" />
+            <Input id="firstname" onChange={(e)=>setFirstName(e.target.value)} placeholder="Khotso" type="text" />
           </LabelInputContainer>
           <LabelInputContainer>
             <Label htmlFor="lastname">Last name</Label>
-            <Input id="lastname" placeholder="Moeketsi" type="text" />
+            <Input id="lastname" onChange={(e)=>setLastname(e.target.value)}  placeholder="Moeketsi" type="text" />
           </LabelInputContainer>
         </div>
         <LabelInputContainer className="mb-4">
+          <Label htmlFor="email">Phone Number</Label>
+          <Input id="number" onChange={(e)=>setNumber(e.target.valueAsNumber)} placeholder="26659749725" type="number" />
+        </LabelInputContainer>
+        <LabelInputContainer className="mb-4">
           <Label htmlFor="email">Email Address</Label>
-          <Input id="email" placeholder="khotso@gmail.com" type="email" />
+          <Input id="email" onChange={(e)=>setEmail(e.target.value)}  placeholder="khotso@gmail.com" type="email" />
         </LabelInputContainer>
         <div className="pt-2 pb-4">
          <Textarea 
-         placeholder="What are you hoping to gain from"
+         onChange={(e)=>setReason(e.target.value)} 
+         placeholder="What are you hoping to gain from this"
          className="resize-none" />
         </div>
 
@@ -49,7 +95,7 @@ export function RegisterCodingInitiative() {
           className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
           type="submit"
         >
-          Sign up &rarr;
+          {!loading ? <>Sign up &rarr;</> : <Loader />}
           <BottomGradient />
         </button>
         <div className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent my-8 h-[1px] w-full" />
