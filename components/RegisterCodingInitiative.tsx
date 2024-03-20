4b17df4d554df2dcde6a0be2c79ade5fa2fe24c9
kali <kali@kali.kali>
2024-03-20 21:51:46 +0200

    one

--- components/RegisterCodingInitiative.tsx
+++ components/RegisterCodingInitiative.tsx
@@ -8,6 +8,7 @@ import {
 } from "@tabler/icons-react";
 import { Label } from "@radix-ui/react-label";
 import { Input } from "./ui/input";
+import { Textarea } from "./ui/textarea";
 
 export function RegisterCodingInitiative() {
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
@@ -38,6 +39,11 @@ export function RegisterCodingInitiative() {
           <Label htmlFor="email">Email Address</Label>
           <Input id="email" placeholder="khotso@gmail.com" type="email" />
         </LabelInputContainer>
+        <div className="pt-2 pb-4">
+         <Textarea 
+         placeholder="What are you hoping to gain from"
+         className="resize-none" />
+        </div>
 
         <button
           className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
@@ -46,30 +52,8 @@ export function RegisterCodingInitiative() {
           Sign up &rarr;
           <BottomGradient />
         </button>
-
         <div className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent my-8 h-[1px] w-full" />
-
         <div className="flex flex-col space-y-4">
-          <button
-            className=" relative group/btn flex space-x-2 items-center justify-start px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]"
-            type="submit"
-          >
-            <IconBrandGithub className="h-4 w-4 text-neutral-800 dark:text-neutral-300" />
-            <span className="text-neutral-700 dark:text-neutral-300 text-sm">
-              GitHub
-            </span>
-            <BottomGradient />
-          </button>
-          <button
-            className=" relative group/btn flex space-x-2 items-center justify-start px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]"
-            type="submit"
-          >
-            <IconBrandGoogle className="h-4 w-4 text-neutral-800 dark:text-neutral-300" />
-            <span className="text-neutral-700 dark:text-neutral-300 text-sm">
-              Google
-            </span>
-            <BottomGradient />
-          </button>
         </div>
       </form>
     </div>
