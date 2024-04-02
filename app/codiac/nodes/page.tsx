32c8712bc5763e8ef8045db1dde4f84d6f2661f6
ragmer09toske <retsepile.raymondshao@gmail.com>
2024-04-01 21:34:06 -0700

    expensive life

--- app/codiac/nodes/page.tsx
+++ app/codiac/nodes/page.tsx
@@ -1,9 +1,11 @@
 "use client"
 import { AnimatedPin } from '@/components/AnimatedPin'
 import React, { useEffect, useState } from 'react'
-import { Card, ProgressBar } from '@tremor/react';
+import { Card, ProgressBar, ProgressCircle } from '@tremor/react';
 import { Navigation } from '@/components/Navigation'
 import axios from 'axios'
+import { RiRecordCircleFill } from '@remixicon/react';
+import { Badge, BadgeDelta } from '@tremor/react';
 
 const Page = () => {
   const [codiacs, setCodiacs] = useState<any[]>([]) // Initialize as empty array
@@ -26,19 +28,38 @@ const Page = () => {
   return (
     <div className='pt-5'>
         <Navigation />
-        <Card className="mx-auto max-w-md">
-      <h4 className="text-tremor-default text-tremor-content dark:text-dark-tremor-content">
-        Sales
-      </h4>
-      <p className="text-tremor-metric font-semibold text-tremor-content-strong dark:text-dark-tremor-content-strong">
-        $71,465
-      </p>
-      <p className="mt-4 flex items-center justify-between text-tremor-default text-tremor-content dark:text-dark-tremor-content">
-        <span>32% of annual target</span>
-        <span>$225,000</span>
-      </p>
-      <ProgressBar value={32} className="mt-2" />
-    </Card>
+
+        <div className="space-y-3">
+        <p className="text-center font-mono text-sm text-slate-500">
+          Codiac Subscribers
+        </p>
+
+        <div className="mx-auto space-y-12">
+          <div className="flex flex-wrap items-center justify-center gap-6">
+            <Badge icon={RiRecordCircleFill}>live</Badge>
+            
+            <BadgeDelta deltaType="moderateIncrease" isIncreasePositive={true}>
+              21.2%
+            </BadgeDelta>
+          </div>
+        </div>
+
+        <Card className="mx-auto max-w-sm">
+          <div className="flex justify-start space-x-5 items-center">
+            <ProgressCircle value={75} size="md">
+              <span className="text-xs font-medium text-slate-700">{arrayLength}</span>
+            </ProgressCircle>
+            <div>
+              <p className="text-tremor-default text-tremor-content-strong dark:text-dark-tremor-content-strong font-medium">
+                Number of codiacs ({arrayLength})
+              </p>
+              <p className="text-tremor-default text-tremor-content dark:text-dark-tremor-content">
+                Spend management control
+              </p>
+            </div>
+          </div>
+        </Card>
+      </div>
 
     </div>
   )
