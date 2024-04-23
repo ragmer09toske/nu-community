d404b2943c19842937202a7f5c139e4f92218a64
kali <kali@kali.kali>
2024-04-23 12:03:09 +0200

    Merge branch 'main' of <remote_repository_url> into main

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,3 @@ import { authMiddleware } from "@clerk/nextjs";
 export default authMiddleware({
    publicRoutes: ['/','/api/uploadthing']
 });
- 
-export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
-};
