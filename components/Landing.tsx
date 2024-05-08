5779a09db57281aaeb5e51ce79be44e999337bbc
ragmer09toske <retsepile.raymondshao@gmail.com>
2024-05-08 14:13:41 +0200

    hello world

--- components/Landing.tsx
+++ components/Landing.tsx
@@ -10,14 +10,15 @@ export const Landing: React.FC = () => {
   return (
     <div className='flex w-full  gap-3 justify-center lg:top-5   items-center lg:h-full h-[120%] fixed bottom-7'>
       <Card style={{
-          backdropFilter: "blur(5px)",
-          borderWidth: "1px",
+        backdropFilter: "blur(5px)",
+        borderWidth: "1px",
       }}
         className="flex justify-center dark:bg-[#09090bd9] lg:w-[80%] mt-16 lg:mt-0 h-[100%] lg:h-[85%] bg-[#FFFFFFFF]"
       >
         {isDesktop && 
           <div className='absolute flex justify-center left-0 p-5'>
             {/* Tenent, hello world */}
+            hello world
           </div>
         }
         <LandingFeed />
