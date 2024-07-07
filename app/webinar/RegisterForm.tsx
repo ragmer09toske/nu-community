138f386dc48fb1dae2a43dd0c49bd4e2373028a3
ragmer09toske <retsepile.raymondshao@gmail.com>
2024-07-07 15:30:14 +0200

    nav bar

--- app/webinar/RegisterForm.tsx
+++ app/webinar/RegisterForm.tsx
@@ -53,8 +53,8 @@ const  RegisterForm = () => {
 
   return (
     <div className="max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-white dark:bg-black">
-      <h2 className="font-bold text-xl text-neutral-800 dark:text-neutral-200">
-        Welcome to Nucleus codiac
+      <h2 className="text-3xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
+        Welcome to Nucleus codiac Webinar
       </h2>
       <p className="text-neutral-600 text-sm max-w-sm mt-2 dark:text-neutral-300">
         You is about to be a nerd
