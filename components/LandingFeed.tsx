515b0392f56aa4d17c071e2243e1635dfed3d03c
ragmer09toske <retsepile.raymondshao@gmail.com>
2024-07-07 11:52:05 +0200

    avatars

--- components/LandingFeed.tsx
+++ components/LandingFeed.tsx
@@ -14,21 +14,21 @@ const people = [
     name: "John Doe",
     designation: "Software Engineer",
     image:
-      "https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80",
+      "/fuma.png",
   },
   {
     id: 2,
     name: "Robert Johnson",
     designation: "Product Manager",
     image:
-      "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YXZhdGFyfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60",
+      "/vee.png",
   },
   {
     id: 3,
     name: "Jane Smith",
     designation: "Data Scientist",
     image:
-      "https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8YXZhdGFyfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60",
+      "/me2.png",
   },
 ];
 
