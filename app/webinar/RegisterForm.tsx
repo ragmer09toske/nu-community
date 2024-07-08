bc3d8d0783e8bc1e75688a39ff9e4216ff264caa
ragmer09toske <retsepile.raymondshao@gmail.com>
2024-07-08 23:33:18 +0200

    questions

--- app/webinar/RegisterForm.tsx
+++ app/webinar/RegisterForm.tsx
@@ -12,10 +12,16 @@ const  RegisterForm = () => {
     setFormType("socialmedia")
   }
   return (
-    <div className="h-full">
-      {formType==="initials" && <InitialsForm />}
-      {formType==="background" && <BackgroundForm />}
-      {formType==="socialmedia" && <SocialmediaProfiles />}
+    <div className="h-full pt-2">
+        <h1 className="text-3xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
+          Nucleus<br /> Codiac Webinar
+      </h1>
+      <div className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent my-8 h-[1px] w-full" />
+      <div className="px-5">
+        {formType==="initials" && <InitialsForm />}
+        {formType==="background" && <BackgroundForm />}
+        {formType==="socialmedia" && <SocialmediaProfiles />}
+      </div>
     </div>
   );
 }
