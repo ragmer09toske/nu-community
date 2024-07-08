c24e97a8e1e96add5179b70a70a17a24efc89a21
ragmer09toske <retsepile.raymondshao@gmail.com>
2024-07-08 22:11:42 +0200

    questions

--- app/webinar/RegisterForm.tsx
+++ app/webinar/RegisterForm.tsx
@@ -319,9 +319,45 @@ const  RegisterForm = () => {
             Welcome to Nucleus codiac Webinar
           </h2>
           <div className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent my-8 h-[1px] w-full" />
-          {formType==="initials" && <InitialsForm />}
-          {formType==="background" && <BackgroundForm />}
-          {formType==="socialmedia" && <SocialmediaProfiles />}
+          <form className="my-8">
+            <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
+              <LabelInputContainer>
+                <Label>First name</Label>
+                <Input onChange={(e)=>setFirstName(e.target.value)} required placeholder="Khotso" type="text" />
+              </LabelInputContainer>
+              <LabelInputContainer>
+                <Label>Last name</Label>
+                <Input  onChange={(e)=>setLastname(e.target.value)} required placeholder="Moeketsi" type="text" />
+              </LabelInputContainer>
+            </div>
+            <LabelInputContainer className="mb-4">
+              <Label>Phone Number</Label>
+              <Input  onChange={(e)=>setNumber(e.target.valueAsNumber)} required placeholder="26659749725" type="number" />
+            </LabelInputContainer>
+            <LabelInputContainer className="mb-4">
+              <Label >Email Address</Label>
+              <Input id="email" onChange={(e)=>setEmail(e.target.value)} required  placeholder="khotso@gmail.com" type="email" />
+            </LabelInputContainer>
+            <div className="pt-2 pb-4">
+            <Textarea 
+             onChange={(e)=>setAbout(e.target.value)} 
+             placeholder="Tell us a little bit about yourself"
+             required
+             className="resize-none" />
+            </div>
+    
+            <button
+              className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
+            >
+              {!loading ? <div onClick={handleNext}>Next &rarr;</div> : <Loader2 className="flex w-full justify-center items-center animate-spin" />}
+              <BottomGradient />
+            </button>
+            <div className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent my-8 h-[1px] w-full" />
+            <div className="flex flex-col space-y-4"></div>
+          </form>
+          {/* {formType==="initials" && <InitialsForm />} */}
+          {/* {formType==="background" && <BackgroundForm />} */}
+          {/* {formType==="socialmedia" && <SocialmediaProfiles />} */}
         </div>
       </ScrollArea>
     </div>
