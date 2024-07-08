41bd719d969c56bbeeadbef65190a1514c20aa7f
ragmer09toske <retsepile.raymondshao@gmail.com>
2024-07-08 17:14:50 +0200

    questions

--- app/webinar/RegisterForm.tsx
+++ app/webinar/RegisterForm.tsx
@@ -295,7 +295,7 @@ const  RegisterForm = () => {
               className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
               onClick={handleNextlinks}
             >
-              {!loading ? <div>Next &rarr;</div> : <Loader2 className="flex w-full justify-center items-center animate-spin" />}
+              {!loading ? <div>Register &rarr;</div> : <Loader2 className="flex w-full justify-center items-center animate-spin" />}
               <BottomGradient />
             </button>
             <div className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent my-8 h-[1px] w-full" />
