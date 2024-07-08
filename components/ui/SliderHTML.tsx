ac050e96949b2ec74634744379a729c411861d22
ragmer09toske <retsepile.raymondshao@gmail.com>
2024-07-09 01:34:21 +0200

    questions

--- components/ui/SliderHTML.tsx
+++ components/ui/SliderHTML.tsx
@@ -1,27 +1,29 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { cn } from "@/lib/utils";
 import { Slider } from "@/components/ui/slider";
+import { WebinarContext } from "@/app/webinar/AppContex";
 
 type SliderProps = React.ComponentProps<typeof Slider>;
 
 export function SliderHTML({ className, ...props }: SliderProps) {
-  const [value, setValue] = useState([50]); // Initial value of the slider
+  let {htmlValue} = useContext(WebinarContext);
+  let {setHtmlValue} = useContext(WebinarContext);
 
   const handleChange = (newValue:any) => {
-    setValue(newValue);
+    setHtmlValue(newValue);
   };
 
   return (
     <div className="w-full flex gap-2 items-center">
       <Slider
-        value={value}
+        value={htmlValue}
         onValueChange={handleChange}
         max={100}
         step={1}
         className={cn("w-[60%]", className)}
         {...props}
       />
-      <div>{value}</div> 
+      <div>{htmlValue}</div> 
     </div>
   );
 }
