24cc33c59e1762aa962a6baa5797151c37d3a240
ragmer09toske <retsepile.raymondshao@gmail.com>
2024-07-09 23:38:21 +0200

    questions

--- app/webinar/Initials.tsx
+++ app/webinar/Initials.tsx
@@ -15,6 +15,13 @@ const InitialsForm = () => {
     let {setAbout} = useContext(WebinarContext);
     let {loading} = useContext(WebinarContext);
 
+    let {firstname} = useContext(WebinarContext);
+    let {jobStatus} = useContext(WebinarContext);
+    let {lastname} = useContext(WebinarContext);
+    let {number} = useContext(WebinarContext);
+    let {about} = useContext(WebinarContext);
+    let {email} = useContext(WebinarContext);
+
     const handleNext = () => {
         setFormType("background")
     }
@@ -48,13 +55,17 @@ const InitialsForm = () => {
            className="resize-none" />
           </div>
   
-          <button
+          { 
+            (firstname && lastname && number && email && about)
+            &&
+            <button
             className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
             onClick={handleNext}
-          >
-            {!loading ? <div>Next &rarr;</div> : <Loader2 className="flex w-full justify-center items-center animate-spin" />}
-            <BottomGradient />
-          </button>
+            >
+              {!loading ? <div>Next &rarr;</div> : <Loader2 className="flex w-full justify-center items-center animate-spin" />}
+              <BottomGradient />
+            </button>
+          }
           <div className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent my-8 h-[1px] w-full" />
           <div className="flex flex-col space-y-4"></div>
         </form>
