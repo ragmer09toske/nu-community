fb9e434427df7e13d332da1a2369538b2962e0fc
ragmer09toske <retsepile.raymondshao@gmail.com>
2024-07-10 16:28:49 +0200

    questions

--- app/webinar/RegisterForm.tsx
+++ app/webinar/RegisterForm.tsx
@@ -1,24 +1,34 @@
 "use client";
 import React, { useContext, useState } from "react";
-import { cn } from "@/app/utils/cn";
 import InitialsForm from "./Initials";
 import BackgroundForm from "./Background";
 import SocialmediaProfiles from "./Socials";
 import { WebinarContext } from "./AppContex";
+import { Card } from "@/components/ui/card";
+import useMobile from "../Mobile";
 
 const  RegisterForm = () => {
   let {formType} = useContext(WebinarContext);
+  const isMobile = useMobile();
+
   return (
     <div className="pt-2">
         <h1 className="text-3xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
           Nucleus<br /> Codiac Webinar
       </h1>
       <div className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent my-8 h-[1px] w-full" />
-      <div className="px-5 lg:p-5">
+      {isMobile ?
+        <div className="px-5 lg:p-5">
           {formType==="initials" && <InitialsForm />}
           {formType==="background" && <BackgroundForm />}
           {formType==="socialmedia" && <SocialmediaProfiles />}
-      </div>
+      </div>:
+      <Card className="px-5 lg:p-5">
+        {formType==="initials" && <InitialsForm />}
+        {formType==="background" && <BackgroundForm />}
+        {formType==="socialmedia" && <SocialmediaProfiles />}
+      </Card>
+      }
     </div>
   );
 }
