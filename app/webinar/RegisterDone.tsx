666d3852b39c5b7a7d004b34f6d3e39fd9cd4545
ragmer09toske <retsepile.raymondshao@gmail.com>
2024-07-10 10:11:41 +0200

    questions

--- app/webinar/RegisterDone.tsx
+++ app/webinar/RegisterDone.tsx
@@ -1,13 +1,16 @@
 import { Button, Dialog, DialogPanel } from '@tremor/react';
-import React, { useContext } from 'react';
-import { WebinarContext } from './AppContex';
+import React from 'react';
 
 export function DialogHero() {
-  let {setIsOpen} = useContext(WebinarContext);
-  let {isOpen} = useContext(WebinarContext);
-
+  const [isOpen, setIsOpen] = React.useState(false);
   return (
     <div className="flex justify-center">
+      <button
+        className="flex items-center justify-center rounded-tremor-small border border-tremor-border bg-tremor-background px-2.5 py-2 text-tremor-default font-medium text-tremor-content-strong shadow-tremor-input hover:bg-tremor-background-muted dark:border-dark-tremor-border dark:bg-dark-tremor-background dark:text-dark-tremor-content-strong dark:shadow-dark-tremor-input hover:dark:bg-dark-tremor-background-muted"
+        onClick={() => setIsOpen(true)}
+      >
+        Show Dialog
+      </button>
       <Dialog
         open={isOpen}
         onClose={() => setIsOpen(false)}
@@ -20,8 +23,7 @@ export function DialogHero() {
             className="mx-auto flex items-center"
             onClick={() => setIsOpen(false)}
           >
-            Registeration Done, See you on the Next step <br/>
-            Thank You for participating
+            Close
           </Button>
         </DialogPanel>
       </Dialog>
