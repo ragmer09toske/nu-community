3fc28af10885f605aa3551b408dbef5f98918bda
ragmer09toske <retsepile.raymondshao@gmail.com>
2024-07-28 00:34:09 +0200

    register Nudevs-Academy(feat): added logout and added some cool ui

--- components/HomeText.tsx
+++ components/HomeText.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import { FlipWords } from "./flip-words";
 
 export function HomeText() {
-  const words = ["better", "cute", "beautiful", "modern"];
+  const words = ["us", "by-us"];
 
   return (
     <div className="h-[40rem] flex justify-center items-center px-4">
       <div className="text-4xl mx-auto font-normal text-neutral-600 dark:text-neutral-400">
-        Build
+        This is
         <FlipWords words={words} /> <br />
-        websites with Aceternity UI
+        Welcome to Nucleusdevs.
       </div>
     </div>
   );
