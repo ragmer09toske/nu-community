42b83ff687ce062851c288d64b0d9ee4714b905c
ragmer09toske <retsepile.raymondshao@gmail.com>
2024-08-06 23:45:03 +0200

    id params

--- app/util/Youthconnect.tsx
+++ app/util/Youthconnect.tsx
@@ -20,7 +20,7 @@ const Youthconnect = () => {
 
   const generateQR = async (id: string) => {
     try {
-      const qrCodeDataURL = await QRCode.toDataURL(`https://nucleusdevs.com/tickets?q=${id}`);
+      const qrCodeDataURL = await QRCode.toDataURL(`https://nucleusdevs.com/util/tickets?q=${id}`);
       setSrc(qrCodeDataURL);
       setTicketId(id);
       setGeneratePdf(true); // Trigger PDF generation
