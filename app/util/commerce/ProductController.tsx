a0198165bc6771d0b3e36a442f1ba8ff7492964c
ragmer09toske <retsepile.raymondshao@gmail.com>
2024-08-12 01:48:56 +0200

    product details

--- app/util/commerce/ProductController.tsx
+++ app/util/commerce/ProductController.tsx
@@ -150,6 +150,7 @@ const ProductController = () => {
       console.error('Error posting product:', error);
     }
   };
+
   return (
     <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
     <div className="mx-auto grid  flex-1 auto-rows-max gap-4">
@@ -631,7 +632,7 @@ const ProductController = () => {
         <Button variant="outline" size="sm">
           Discard
         </Button>
-        <Button size="sm">Save Product</Button>
+        <Button size="sm" onClick={postProduct}>Save Product</Button>
       </div>
     </div>
   </main>
