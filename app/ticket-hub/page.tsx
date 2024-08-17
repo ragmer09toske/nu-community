4eb0c9aa3cf027e0f259edbe395ca1e3987a8011
ragmer09toske <retsepile.raymondshao@gmail.com>
2024-08-17 23:24:22 +0200

    main

--- app/ticket-hub/page.tsx
+++ app/ticket-hub/page.tsx
@@ -108,7 +108,7 @@ export default function Example() {
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <a
-                href="#"
+                href="#collectins"
                 className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 Get started
@@ -134,7 +134,9 @@ export default function Example() {
       </div>
       <People />
       <MentoShipPricing />
-      <Collections />
+      <div id='collectins'>
+        <Collections />
+      </div>
       <CTA />
       <TicketDetails />
       <Footer />
