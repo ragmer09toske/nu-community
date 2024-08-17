ce54888f3ae3fb179d1881b159dc85c8d9df628c
ragmer09toske <retsepile.raymondshao@gmail.com>
2024-08-18 01:41:48 +0200

    main

--- app/ticket-hub/Shop/AuthorCards.tsx
+++ app/ticket-hub/Shop/AuthorCards.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import Image from "next/image";
 
@@ -8,7 +9,7 @@ export function AuthorCard() {
       <div
         className={cn(
           " cursor-pointer overflow-hidden relative card h-96 rounded-md shadow-xl  max-w-sm mx-auto backgroundImage flex flex-col justify-between p-4",
-          "bg-[url(https://images.unsplash.com/photo-1544077960-604201fe74bc?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1651&q=80)] bg-cover"
+          "bg-[url(/Thumbnail.png)] bg-cover"
         )}
       >
         <div className="absolute w-full h-full top-0 left-0 transition duration-300 group-hover/card:bg-black opacity-60"></div>
@@ -28,13 +29,7 @@ export function AuthorCard() {
           </div>
         </div>
         <div className="">
-          <h1 className="font-bold text-xl md:text-2xl text-gray-50 relative left-0 z-10">
-            Next.js
-          </h1>
-          <p className="font-normal text-sm text-gray-50 relative z-10 my-4">
-            Card with Author avatar, complete name and time to read - most
-            suitable for blogs.
-          </p>
+          <Button>View</Button>
         </div>
       </div>
     </div>
