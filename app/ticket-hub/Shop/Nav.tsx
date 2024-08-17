725a4514029dabf5199e90d4ee9ab99e26cad92d
ragmer09toske <retsepile.raymondshao@gmail.com>
2024-08-17 23:04:42 +0200

    main

--- app/ticket-hub/Shop/Nav.tsx
+++ app/ticket-hub/Shop/Nav.tsx
@@ -145,15 +145,15 @@ export default function Example() {
   const [open, setOpen] = useState(false)
 
   return (
-    <div className="bg-white">
+    <div className="w-full">
       {/* Mobile menu */}
-      <header className="relative bg-white">
+      <header className="relative w-full">
         <p className="flex h-10 items-center justify-center bg-indigo-600 px-4 text-sm font-medium text-white sm:px-6 lg:px-8">
           Get free delivery on orders over $100
         </p>
 
         <nav aria-label="Top" className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-          <div className="border-b border-gray-200">
+          <div className="" >
             <div className="flex h-16 items-center">
               <button
                 type="button"
@@ -214,6 +214,7 @@ export default function Example() {
               </div>
             </div>
           </div>
+          <div className="-mt-2.5 bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-300 to-transparent my-8 h-[1px] w-full" />
         </nav>
       </header>
     </div>
