82c40ba44d8b84d559c8f641a07c22721b086908
Retsepile Shao <73499664+ragmer09toske@users.noreply.github.com>
2024-09-28 00:25:21 +0200

    all

--- crotia/app/page.tsx
+++ crotia/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import Example from "@/app/util/JustCode";
 import Navbar_Codiac from "@/components/CodiacNav";
+import { FloatingNavbar } from "@/components/home/Nav";
 import Home_Data_One_Repo from "@/components/Home_Data_One_Repo";
 import Navbar from "@/components/Navbar";
 import PricingComponent from "@/components/Pricing";
@@ -8,7 +9,8 @@ import PricingComponent from "@/components/Pricing";
 const Home = () => {
   return (
       <div>
-        <Navbar />
+        {/* <Navbar /> */}
+        <FloatingNavbar />
         <Home_Data_One_Repo />
         <div className="h-24 p-10"></div>
       </div>
