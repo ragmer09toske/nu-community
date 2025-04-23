45d30ce9df307009a11a4f73d8771b7bfe47c862
Retsepile Shao <73499664+ragmer09toske@users.noreply.github.com>
2025-04-23 12:54:34 +0200

    all:

---
+++ app/projects/[username]/page.tsx
@@ -0,0 +1,36 @@
+import Navbar from "@/components/Navbar";
+import { Spotlight } from "@/components/Spotlight";
+
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ username: string }>;
+}) {
+  const { username } = await params;
+  return (
+    <div>
+      <Navbar />
+      <div className="relative isolate px-6 lg:px-8 h-full">
+        <div
+          className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+          aria-hidden="true"
+        >
+          <div
+            className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-r from-slate-500 to-slate-300 opacity-20 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+            style={{
+              clipPath:
+                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+            }}
+          ></div>
+        </div>
+        <div className="relative">
+          <section>
+            <div className="max-w-screen-xl mx-auto px-4 pt-5 gap-5 text-gray-600 overflow-hidden md:px-8 md:flex">
+              {username} is the username
+            </div>
+          </section>
+        </div>
+      </div>
+    </div>
+  );
+}
